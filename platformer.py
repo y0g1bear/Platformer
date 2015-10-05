5d422e410f9da1eab0c949463c468297b9257122
Eric Dennison <ericd@netdenizen.com>
2015-10-05 13:55:03 -0400

    Create platformer.py

---
+++ platformer.py
@@ -0,0 +1,8 @@
+"""
+platformer.py
+Author: <your name here>
+Credit: <list sources used, if any>
+Assignment:
+Write and submit a program that implements the sandbox platformer game:
+https://github.com/HHS-IntroProgramming/Platformer
+"""
