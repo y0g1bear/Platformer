d43ff48c71db6f1757cda81c95a4e8d7183d3107
y0g1bear <johnwarhold@hanovernorwichschools.org>
2016-11-28 13:59:55 -0500

    Updated from Brython Server: 11/28/2016 1:59:58 PM

--- platformer.py
+++ platformer.py
@@ -6,3 +6,7 @@ Assignment:
 Write and submit a program that implements the sandbox platformer game:
 https://github.com/HHS-IntroProgramming/Platformer
 """
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
+Screen_width = 640
+Screen_height = 480
