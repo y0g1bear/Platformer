e2673ec1baec455a5b802bc145ed8bdbc9cf1fcc
y0g1bear <johnwarhold@hanovernorwichschools.org>
2016-11-29 13:41:56 -0500

    Updated from Brython Server: 11/29/2016 1:42:00 PM

--- platformer.py
+++ platformer.py
@@ -8,5 +8,24 @@ https://github.com/HHS-IntroProgramming/Platformer
 """
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
 
-Screen_width = 640
-Screen_height = 480
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+blueviolet = Color(0x8a2be2, 1.0)
+hotpink = Color(0xff69b4, 1.0)
+Aquamarine =Color(0x7fffd4, 1.0)
+brown = Color(0xcd5c5c, 1.0)
+green = Color(0x00ff7f, .5)
+thist = Color(0xd8bfd8, 1.0)
+white = Color(0xffffff, 1.0)
+noline = LineStyle(1,white)
+
+# Background
+black = Color(0, 1)
+noline = LineStyle(1, Aquamarine)
+bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, white)
+bg = Sprite(bg_asset, (0,0))
+
+
+myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
+myapp.run()
