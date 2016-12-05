48035942c4fcff43f879d1e2e95309a4d97a50d4
y0g1bear <johnwarhold@hanovernorwichschools.org>
2016-12-05 13:58:28 -0500

    Updated from Brython Server: 12/5/2016 1:58:27 PM

--- platformer.py
+++ platformer.py
@@ -17,7 +17,7 @@ Aquamarine =Color(0x7fffd4, 1.0)
 brown = Color(0xcd5c5c, 1.0)
 green = Color(0x00ff7f, .5)
 thist = Color(0xd8bfd8, 1.0)
-white = Color(0xffffff, 1.0)
+white = Color(0xfafafa, 1.0)
 noline = LineStyle(1,white)
 
 # Background
@@ -25,6 +25,17 @@ black = Color(0, 1)
 noline = LineStyle(1, Aquamarine)
 bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, white)
 bg = Sprite(bg_asset, (0,0))
+
+class suasage(Sprite):
+    """
+    suasage
+    """
+    asset = ImageAsset("sausage-merguez.jpg", 
+        Frame(227,0,292-227,125), 4, 'vertical')
+
+    def __init__(self, position):
+        super().__init__(SpaceShip.asset, position)
+SpaceShip((100,100))
 
 
 myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
